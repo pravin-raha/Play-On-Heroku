c876c97f38acd66e88388dfc122287d236865b34
pravin <pravin.rahangdale@synerzip.com>
2016-07-21 15:54:38 +0530

    Initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+/**
+  * Created by synerzip1 on 21/7/16.
+  */
+class HomeController extends Controller {
+
+  def index = Action {
+    Ok("sadfgj")
+  }
+
+}
